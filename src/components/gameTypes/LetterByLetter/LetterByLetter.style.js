f2f72fd11fd9174e5b546736dfaaeb786bf87c21
Paweł Miłczak <jogobello4@gmail.com>
2020-09-25 22:04:46 +0200

    Add @media to LetterByLetter

--- src/components/gameTypes/LetterByLetter/LetterByLetter.style.js
+++ src/components/gameTypes/LetterByLetter/LetterByLetter.style.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import { flexColumnCenter } from "theme/mixins";
-import { fontSize } from "theme/theme";
+import { breakPoints, fontSize } from "theme/theme";
 
 export const MainWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr auto 1fr;
+  justify-items: center;
   align-items: center;
-  justify-content: space-between;
+  padding-top: 70px;
   height: 100%;
   max-height: 600px;
   width: 100%;
@@ -16,29 +18,49 @@ export const MainWrapper = styled.div`
 export const WordToFill = styled.div`
   display: flex;
   flex-direction: row;
+  height: 50px;
 
   div {
-    width: 15px;
+    width: 10px;
     margin: 0 5px;
     text-align: center;
-    font-size: ${fontSize.xl};
+    font-size: ${fontSize.l};
+  }
+
+  @media ${breakPoints.tablet} {
+    div {
+      width: 15px;
+      font-size: ${fontSize.xl};
+    }
   }
 `;
 
 export const LettersWrapper = styled.div`
   display: flex;
-  max-width: 500px;
+  max-width: 90%;
+
+  @media ${breakPoints.tablet} {
+    max-width: 500px;
+  }
 `;
 
 export const LetterItem = styled.div`
-  width: 60px;
-  height: 80px;
-  margin: 0 15px;
-  border: 3px solid white;
-  border-radius: 14px;
+  width: 50px;
+  height: 65px;
+  margin: 0 5px;
+  border: 2px solid white;
+  border-radius: 10px;
   text-transform: uppercase;
   ${flexColumnCenter};
-  font-size: ${fontSize.xl};
+  font-size: ${fontSize.l};
   cursor: pointer;
   opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+
+  @media ${breakPoints.tablet} {
+    font-size: ${fontSize.xl};
+    border-radius: 14px;
+    width: 60px;
+    height: 80px;
+    margin: 0 15px;
+  }
 `;
