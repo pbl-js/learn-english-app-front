3324f7a0743224299f6667a2d81b0e5a956edc9d
Paweł Miłczak <jogobello4@gmail.com>
2020-09-25 13:52:12 +0200

    Add auth redirect for login user

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,23 +1,31 @@
-import React, { useState } from "react";
-import Link from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Redirect } from "react-router-dom";
+import routes from "router/routes";
 
-import { GradientWrapper, ModalWrapper, Form } from "./Auth.style";
+import { AuthContext } from "context/AuthContext";
+
+import { GradientWrapper, ModalWrapper } from "./Auth.style";
 import AuthHeader from "components/AuthHeader/AuthHeader";
 import Register from "components/Register/Register";
 import Login from "components/Login/Login";
 
 const Auth = () => {
   const [authType, setAuthType] = useState("login");
+  const { user } = useContext(AuthContext);
 
-  return (
-    <GradientWrapper color="orange">
-      <ModalWrapper>
-        <AuthHeader authType={authType} setAuthType={setAuthType} />
+  if (user) {
+    return <Redirect to={routes.app} />;
+  } else {
+    return (
+      <GradientWrapper color="orange">
+        <ModalWrapper>
+          <AuthHeader authType={authType} setAuthType={setAuthType} />
 
-        {authType === "register" ? <Register /> : <Login />}
-      </ModalWrapper>
-    </GradientWrapper>
-  );
+          {authType === "register" ? <Register /> : <Login />}
+        </ModalWrapper>
+      </GradientWrapper>
+    );
+  }
 };
 
 export default Auth;
